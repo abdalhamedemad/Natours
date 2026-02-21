58b40c75e635f07808884b1dffaf14a674c214d4
abdalhamedemad <76442606+abdalhamedemad@users.noreply.github.com>
2026-02-21 15:50:14 +0200

    feat: add global error handler

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import tourRouter from './routes/tourRoutes';
@@ -7,6 +7,12 @@ import userRouter from './routes/userRoutes';
 interface CustomRequest extends Request {
   requestTime?: string;
 }
+
+export interface AppError extends Error {
+  statusCode?: number;
+  status?: 'fail' | 'error';
+}
+
 const app = express();
 
 // 1) MIDDLEWARE
@@ -27,11 +33,11 @@ app.use(express.static(path.join(__dirname, '../public')));
 
 // this a middle ware that will execute for every/any request bec we do not
 // specify a route and before any of the routes
-app.use((req, res, next) => {
-  console.log('hello from middleware');
-  // if we do not call the next method req will stuck here
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('hello from middleware');
+//   // if we do not call the next method req will stuck here
+//   next();
+// });
 //  this middleware add a new property to the request object
 app.use((req: CustomRequest, res: Response, next) => {
   req.requestTime = new Date().toISOString();
@@ -45,5 +51,42 @@ app.use((req: CustomRequest, res: Response, next) => {
 // mounting the router to a specific url
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// here will be reached if the request did not handled by any of the above handlers
+// all means all the types of the request get,post,patch....
+// this works fine in express before version 5 but in this version error handling
+// writes as a middle ware
+// app.all('/*', (req, res, next) => {
+//   res.status(404).json({
+//     status: 'fail',
+//     message: `Can't find ${req.originalUrl} on this server!`,
+//   });
+// });
+app.use((req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  // next();
+  const err = new Error(
+    `Can't find ${req.originalUrl} on this server!`,
+  ) as AppError;
+  err.status = 'fail';
+  err.statusCode = 404;
+  //  in Express if we passed an arguments to next function automatically will know that is an err
+  // so will go and executed the error middleware
+  next(err);
+});
+
+// Global error handling here will handles the operational errors so only here will send the erors and in the other palce we just throws it
+// this middleware is an error middleware has 4 params , when a errors occurs express will call this middleware
+app.use((err: AppError, req: Request, res: Response, next: NextFunction) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 // 4) START SERVER
 export default app;
